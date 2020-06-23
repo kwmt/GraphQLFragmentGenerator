71e68eed480cca742f20143da9d875433f6301b5
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-06-24 02:04:20 +0900

    delete unused files

--- graphql_generator/lib/graphql_generator.dart
+++ graphql_generator/lib/graphql_generator.dart
@@ -1,11 +1,7 @@
 library graphql_generator;
 import 'package:build/build.dart';
-import 'package:graphql_generator/src/enum_has_value_generator.dart';
 import 'package:graphql_generator/src/graphql_fragment_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-
-//Builder enumHasValueBuilder(BuilderOptions options) =>
-//    PartBuilder([EnumHasValueGenerator()], ".value.g.dart");
 Builder graphQLFragmentBuilder(BuilderOptions options) =>
     PartBuilder([GraphQLFragmentGenerator()], ".graphql.g.dart");
